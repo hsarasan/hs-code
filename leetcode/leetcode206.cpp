0fc356f620f30034160d2085b52a123240951d08
cppmonkey75 <hari_sarasan@yahoo.com>
2024-03-20 23:44:00 +0000

    Update leetcode206.cpp

--- leetcode/leetcode206.cpp
+++ leetcode/leetcode206.cpp
@@ -1,3 +1,5 @@
+//vector based solution
+
 class Solution {
     
 public:
@@ -18,5 +20,32 @@ public:
         }
         );
         return head;;
+    }
+};
+
+////////////////recursive solution
+class Solution {
+    ListNode * reverse(ListNode *head){
+        auto node=head;
+        if (!node) return node;
+        if (!node->next) return node;
+        if (!node->next->next){
+            node->next->next=node;
+            node=node->next;
+            node->next->next=nullptr;
+            return node;  
+        }
+        auto temp_head=head->next;
+        temp_head=reverse(temp_head);
+        auto tail=temp_head;
+        while(tail->next) tail=tail->next;
+        tail->next=head;
+        head->next=nullptr;
+        head=temp_head;
+        return head;
+    }
+public:
+    ListNode* reverseList(ListNode* head) {
+        return reverse(head);
     }
 };
