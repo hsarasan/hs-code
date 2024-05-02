3e35fbb6b556fa66b9643cf6e5b0f3ec51fa8b4c
cppmonkey75 <hari_sarasan@yahoo.com>
2024-05-02 06:11:26 +0100

    Create crtp.cpp

---
+++ cpp-sprint/crtp.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+template <typename T>
+class Base{
+    public:
+    void f(){
+        static_cast<T*> (this)->f2();
+    }
+};
+
+class Derived: public Base<Derived>{
+    public:
+    void f2(){
+        std::cout<<"Derived:f()"<<std::endl;
+    }
+};
+class Derived2: public Base<Derived>{
+    public:
+    void f2(){
+        std::cout<<"Derived2:f()"<<std::endl;
+    }
+};
+
+int main(){
+    Derived d;
+    d.f();
+    Derived d2;
+    d2.f();
+}
