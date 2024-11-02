8e3f338c96eda929712cb26668cc23d0cf3bb35e
Hari <hari_sarasan@yahoo.com>
2024-11-02 17:38:21 +0000

    Create sum.cpp

---
+++ cpp-revision/templates/sum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <type_traits>
+#include <string>
+
+using namespace std;
+
+/*
+template <typename T>
+T minc(T arg1, T arg2){
+
+    return arg1<arg2?arg1:arg2;
+
+}
+*/
+
+
+
+template <typename T, typename... Args>
+T sum(T a, Args... args)
+{
+   if constexpr (sizeof...(args) == 0)
+      return a;
+   else
+      return a + sum(args...);
+}
+int main(){
+    cout<<sum(7,4,3,2)<<endl;
+    cout<<sum(string("def"),string("abc"),string("jki"))<<endl;
+    cout<<sum(7,9)<<endl;
+}
