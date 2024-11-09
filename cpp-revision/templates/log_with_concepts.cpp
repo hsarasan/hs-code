fff2433b32fd84d4180e3773feef2c95db976fde
cppmonkey75 <66827205+cppmonkey75@users.noreply.github.com>
2024-11-09 23:11:04 +0000

    xxx

---
+++ cpp-revision/templates/log_with_concepts.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <list>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+template <typename T>
+concept associative_container = requires (T t){
+    typename T::value_type;
+    typename T::key_type;
+    typename T::mapped_type;
+};
+template <typename T>
+concept sequence_container = requires (T t){
+    typename T::value_type;
+    t.begin();
+    t.end();
+} && !associative_container<T>;
+
+template <associative_container C>
+void log(const C & c){
+    for (const auto & [k,v]: c)
+        cout<<k<<"=>"<<v<<" ";
+    cout<<endl;
+};
+template <sequence_container C>
+void log (const C & c){
+    using T = C::value_type;
+    copy(c.begin(), c.end(), ostream_iterator<T>(cout,","));
+    cout<<endl;
+} ;
+
+
+int main(){
+    vector<int> v{1,2,3,4};
+    map<int,string> m{ {1,"one"},{2,"two"}};
+    log(v);
+    log(m);
+    return 0;
+}
