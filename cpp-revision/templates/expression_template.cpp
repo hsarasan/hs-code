65580aea214350effb1efc298f02b0bb3ed1bc83
cppmonkey75 <66827205+cppmonkey75@users.noreply.github.com>
2024-11-16 20:49:08 +0000

    Update expression_template.cpp

--- cpp-revision/templates/expression_template.cpp
+++ cpp-revision/templates/expression_template.cpp
@@ -8,7 +8,7 @@ struct Adder{
     const L & l;
     const R & r;
     Adder(const L& left, const R& right):l(left),r(right){}
-    int operator[](size_t i) const { 
+    auto operator[](size_t i) const { 
         return l[i]+r[i];
     }
     size_t size() const { return l.size(); };
@@ -21,7 +21,7 @@ Adder<L,R> operator+(const L& l, const R& r){
 
 int main(){
     vector<int> v1{1,2,3,4,5};
-    vector<int> v2{1,2,3,4,5};
+    vector<double> v2{1.1,22,3.3,4.5,5};
     vector<int> v3{1,2,3,4,5};
     auto v = v1+v2+v3;
     for (int i=0;i<v.size();++i){
