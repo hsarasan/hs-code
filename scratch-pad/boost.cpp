3540fb3a549f98988ca2a9482573885bbb79ecbc
cppmonkey75 <66827205+cppmonkey75@users.noreply.github.com>
2025-01-12 16:17:20 +0000

    rearranging code

--- scratch-pad/boost.cpp
+++
@@ -1,9 +0,0 @@
-#include <boost/algorithm/string.hpp>
-#include <iostream>
-
-int main() {
-    std::string str = "Boost Libraries";
-    boost::to_upper(str);
-    std::cout << str << std::endl;  // Should output "BOOST LIBRARIES"
-    return 0;
-}
