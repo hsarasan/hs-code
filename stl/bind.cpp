fe4222145a6a3b5ac0f442e82ee250fe1129a9f8
cppmonkey75 <66827205+cppmonkey75@users.noreply.github.com>
2025-01-12 16:09:37 +0000

    rearranging code

--- stl/bind.cpp
+++ stl/bind.cpp
@@ -1,21 +1,14 @@
 #include <iostream>
-#include <forward_list>
 #include <algorithm>
-#include <numeric>
-#include <iterator>
-#include <vector>
-#include <list>
-#include <string>
 #include <functional>
-#include <queue>
 
 using namespace std;
 using namespace std::placeholders;
 
-int main(){
-    vector<int> v{1,2,3,4};
-    vector<int> v2;
-    transform(v.begin(), v.end(), back_inserter(v2), bind(multiplies<int>(),_1,10));
-    copy(v2.begin(), v2.end(), ostream_iterator<int>(cout,","));
-}
+int add(int a, int b){ return a+b;}
 
+int main(){
+    auto add5=std::bind(add,5, placeholders::_1);
+    cout<<add5(10)<<endl;
+    return 0;
+}
