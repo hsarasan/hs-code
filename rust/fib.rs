df2856a3e7cd222e3d1ca060bf28a22536a6097c
cppmonkey75 <hari_sarasan@yahoo.com>
2025-02-01 11:01:04 +0000

    testing new repo

--- rust/fib.rs
+++ rust/fib.rs
@@ -1,11 +1,11 @@
 fn fibonacci(n: u32) -> u32 {
     if n <= 1 {
-        return n;
+        return 1;
     }
     return fibonacci(n - 1) + fibonacci(n - 2)
 }
 
 fn main() {
-    let n = 10; // You can change this value to calculate a different Fibonacci number
+    let n = 5; // You can change this value to calculate a different Fibonacci number
     println!("Fibonacci number at position {} is {}", n, fibonacci(n));
 }
