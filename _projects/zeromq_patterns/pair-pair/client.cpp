423ed2cfce5c2343941b379f360905b24bde1347
cppmonkey75 <hari_sarasan@yahoo.com>
2025-02-24 22:49:20 +0000

    a few changes to client & server

--- _projects/zeromq_patterns/pair-pair/client.cpp
+++ _projects/zeromq_patterns/pair-pair/client.cpp
@@ -3,25 +3,33 @@
 #include <thread>
 #include <chrono>
 
+void sender(zmq::socket_t &socket){
+
+	while(true){
+        std::string message = "Hello from Client";
+        socket.send(zmq::message_t(message), zmq::send_flags::none);
+        std::cout << "[CLIENT] Sent: " << message << std::endl;
+				std::this_thread::sleep_for(std::chrono::seconds(2));
+	}
+}
+void receiver(zmq::socket_t &socket){
+	while(true){
+        // Receive response
+        zmq::message_t reply;
+        socket.recv(reply, zmq::recv_flags::none);
+        std::cout << "[CLIENT] Received: " << std::string(static_cast<char*>(reply.data()), reply.size()) << std::endl;
+	}
+}
+
 int main() {
     zmq::context_t context(1);
     zmq::socket_t socket(context, zmq::socket_type::pair);
     socket.connect("tcp://localhost:5555");
 
-    std::cout << "P2P Client connected...\n";
-
-    while (true) {
-        std::string message = "Hello from Client";
-        socket.send(zmq::message_t(message), zmq::send_flags::none);
-        std::cout << "[CLIENT] Sent: " << message << std::endl;
-
-        // Receive response
-        zmq::message_t reply;
-        socket.recv(reply, zmq::recv_flags::none);
-        std::cout << "[CLIENT] Received: " << std::string(static_cast<char*>(reply.data()), reply.size()) << std::endl;
-
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-    }
+		std::thread send_thr(sender,std::ref(socket));
+		std::thread recv_thr(receiver,std::ref(socket));
+		send_thr.join();
+		recv_thr.join();
 
     return 0;
 }
--- _projects/zeromq_patterns/pair-pair/server.cpp
+++ _projects/zeromq_patterns/pair-pair/server.cpp
@@ -3,26 +3,30 @@
 #include <thread>
 #include <chrono>
 
+void server_side( zmq::socket_t & socket){
+		while(true){
+    	zmq::message_t request;
+    	socket.recv(request, zmq::recv_flags::none);
+    	std::string msg(static_cast<char*>(request.data()), request.size());
+
+    	std::cout << "[SERVER] Received: " << msg << std::endl;
+
+    	// Send response back
+    	std::this_thread::sleep_for(std::chrono::seconds(1));
+    	std::string reply = "Reply from Server";
+    	socket.send(zmq::message_t(reply), zmq::send_flags::none);
+    	std::cout << "[SERVER] Sent reply\n";
+		}
+}
+
 int main() {
     zmq::context_t context(1);
     zmq::socket_t socket(context, zmq::socket_type::pair);
     socket.bind("tcp://*:5555");
-
     std::cout << "P2P Server started...\n";
 
-    while (true) {
-        zmq::message_t request;
-        socket.recv(request, zmq::recv_flags::none);
-        std::string msg(static_cast<char*>(request.data()), request.size());
-
-        std::cout << "[SERVER] Received: " << msg << std::endl;
-
-        // Send response back
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::string reply = "Reply from Server";
-        socket.send(zmq::message_t(reply), zmq::send_flags::none);
-        std::cout << "[SERVER] Sent reply\n";
-    }
+		std::thread tid(server_side, std::ref(socket));
+		tid.join();
 
     return 0;
 }
