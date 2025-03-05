9c7ab20c42a5dc4ea0a445e68e7d39ade24ea569
cppmonkey75 <66827205+cppmonkey75@users.noreply.github.com>
2025-03-05 18:31:04 +0000

    Update solution.py

--- daily-coding-problem/13/solution.py
+++ daily-coding-problem/13/solution.py
@@ -1,22 +1,13 @@
 def find_biggest_substring(s,k):
     start=0
-    end=0
     char_set=set()
     max_len=1
-    char_set.add(s[0])
-    while end<len(s):
-        c=s[end]
-        char_set.add(c)
-        if len(char_set)<k+1:
-            max_len=max(max_len,end-start+1)
-            print(s[start:end+1])
-            end+=1
-        else:
-            start=start+1
-            end=start
-            char_set.clear()
-            if start<len(s):
-                char_set.add(s[start])
+    for end in range(len(s)):
+        char_set.add(s[end])
+        while len(char_set)>k:
+            char_set.remove(s[start])
+            start+=1
+        max_len=max(max_len,end-start+1)
     return max_len
     
 print(find_biggest_substring("abcba",2))
