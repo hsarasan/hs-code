9c7eef85b5a83046814121d5c27d55310216f86a
cppmonkey75 <66827205+cppmonkey75@users.noreply.github.com>
2025-03-06 20:09:21 +0000

    value of pi with Monte Carlo

---
+++ daily-coding-problem/14/solution.py
@@ -0,0 +1,10 @@
+import random
+n=0
+inside_circle=0
+for i in range(100000):
+    x=random.uniform(-10,10)
+    y=random.uniform(-10,10)
+    if x*x+y*y<=100:
+        inside_circle+=1
+    n+=1
+print(4*inside_circle/n)
