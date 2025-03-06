fd66cdbb2f32c8a485512dda36238e1eba62a2cc
cppmonkey75 <66827205+cppmonkey75@users.noreply.github.com>
2025-03-06 20:09:43 +0000

    Update solution.py

--- daily-coding-problem/14/solution.py
+++ daily-coding-problem/14/solution.py
@@ -2,7 +2,7 @@ import random
 n=0
 inside_circle=0
 for i in range(100000):
-    x=random.uniform(-10,10)
+    x=random.uniform(-10,10) #use uniform to generate floating point instead of integers
     y=random.uniform(-10,10)
     if x*x+y*y<=100:
         inside_circle+=1
